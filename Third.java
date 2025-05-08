c174e03b8161f32448793f6bec62e5caba4de0fa
habiba25-4 <163042672+habiba25-4@users.noreply.github.com>
2025-05-08 20:38:28 +0300

    Create Third.java

---
+++ Third.java
@@ -0,0 +1,8 @@
+public class Third {
+    private String message;
+    
+    public Third() {
+        this.message = "Third class constructor called";
+        System.out.println(message);
+    }
+}
